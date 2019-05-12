df3ccc5d7ecb4c2e97fa6cc7181c28bc94e99339
Thomas Lorreyte <tom.lorreyte@gmail.com>
2019-05-12 16:59:07 +0000

    Bugs corrected

--- src/Brady_Tachy.m
+++ src/Brady_Tachy.m
@@ -1,4 +1,4 @@
-function [ result ] = Brady_Tachy( R2, Fs )
+function [ result, bpm ] = Brady_Tachy( R2, Fs )
 %% Detect Brady or Tachycardia with R-pics position
 
 delta_RR=[];
@@ -6,6 +6,8 @@ for i=1:length(R2)-1
     delta_RR = [delta_RR R2(i+1)-R2(i)];
 end
 delta_barre = mean(delta_RR);
+
+bpm = 1/delta_barre*Fs*60;
 
 if (1/delta_barre*Fs*60)<60
     result='Bradycardia';
